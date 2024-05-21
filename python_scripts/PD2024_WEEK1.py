dffe2bbe913c446452579e2b26e4ac9337c13735
Gabriela Mourão <38528326+gcmourao@users.noreply.github.com>
2024-05-21 18:43:52 +0400

    add geolocation

--- python_scripts/PD2024_WEEK1.py
+++ python_scripts/PD2024_WEEK1.py
@@ -1,5 +1,9 @@
 import pandas as pd
 from pathlib import Path
+import numpy as np
+from geopy.geocoders import Nominatim
+
+geolocator = Nominatim(user_agent="MyApp")
 main_file_path = Path(__file__).resolve().parent.parent
 
 
@@ -7,7 +11,7 @@ main_file_path = Path(__file__).resolve().parent.parent
 input_file_path = main_file_path / 'input_files/PD 2024 Wk 1 Input.csv'
 input_file = pd.read_csv(input_file_path)
 
-# Break 'Flight details into columns
+# Break 'Flight details' into columns
 input_file[['Date', 'Flight Number', 'From-To', 'Class', 'Price']] = input_file['Flight Details'].str.split('//',
                                                                                                             expand=True)
 input_file[['From', 'To']] = input_file['From-To'].str.split('-', expand=True)
@@ -22,11 +26,25 @@ input_file['Flow Card?'] = input_file['Flow Card?'].apply(lambda x: 'Yes' if x =
 input_file = input_file[['Date', 'Flight Number', 'From', 'To', 'Class', 'Price', 'Flow Card?', 'Bags Checked',
                          'Meal Type']]
 
+# Extra step for visualization in tableau
+cities = input_file["From"].unique()
+def input_geolocation(list_of_cities, df, input):
+    df[input + '_latitude'] = [None]*df.shape[0]
+    df[input + '_longitude'] = [None] * df.shape[0]
+    for item in list_of_cities:
+        latitude = geolocator.geocode(item).latitude
+        longitude = geolocator.geocode(item).longitude
+        df[input + '_latitude'] = np.where(df[input] == item, latitude, df[input + '_latitude'])
+        df[input + '_longitude'] = np.where(df[input] == item, longitude, df[input + '_longitude'])
+    return df
+input_file = input_geolocation(cities, input_file, 'From')
+input_file = input_geolocation(cities, input_file, 'To')
+
 # Creating output files
 output_has_flow_card = input_file[input_file['Flow Card?'] == 'Yes']
 output_no_flow_card = input_file[input_file['Flow Card?'] == 'No']
 
-# Check files
+# Check files. Note that it will have 4 extra columns when compared to the official challenge answer
 print(output_has_flow_card.T)
 print(output_has_flow_card.shape)
 print(output_no_flow_card.T)
@@ -35,3 +53,7 @@ print(output_no_flow_card.shape)
 # Save files
 output_has_flow_card.to_csv(main_file_path / 'output_files/P2024Week1_withFlowCard.csv', index=False)
 output_no_flow_card.to_csv(main_file_path / 'output_files/P2024Week1_withoutFlowCard.csv', index=False)
+
+
+
+
