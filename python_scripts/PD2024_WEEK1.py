c06a9f4df456fbf9420c73e58276ffe0b7fe38db
Gabriela Mourão <38528326+gcmourao@users.noreply.github.com>
2024-05-21 13:45:14 +0400

    Initial commit

---
+++ python_scripts/PD2024_WEEK1.py
@@ -0,0 +1,37 @@
+import pandas as pd
+from pathlib import Path
+main_file_path = Path(__file__).resolve().parent.parent
+
+
+# Read the data from the challenge file
+input_file_path = main_file_path / 'input_files/PD 2024 Wk 1 Input.csv'
+input_file = pd.read_csv(input_file_path)
+
+# Break 'Flight details into columns
+input_file[['Date', 'Flight Number', 'From-To', 'Class', 'Price']] = input_file['Flight Details'].str.split('//',
+                                                                                                            expand=True)
+input_file[['From', 'To']] = input_file['From-To'].str.split('-', expand=True)
+input_file.drop(columns=['Flight Details', 'From-To'], inplace=True)
+
+# adjust date, price format and Flow card
+input_file['Date'] = pd.to_datetime(input_file['Date']).dt.strftime('%d/%m/%Y')
+input_file['Price'] = input_file['Price'].astype(float).apply(lambda x: '{:,.2f}'.format(x))
+input_file['Flow Card?'] = input_file['Flow Card?'].apply(lambda x: 'Yes' if x == 1 else 'No')
+
+# reorder the columns
+input_file = input_file[['Date', 'Flight Number', 'From', 'To', 'Class', 'Price', 'Flow Card?', 'Bags Checked',
+                         'Meal Type']]
+
+# Creating output files
+output_has_flow_card = input_file[input_file['Flow Card?'] == 'Yes']
+output_no_flow_card = input_file[input_file['Flow Card?'] == 'No']
+
+# Check files
+print(output_has_flow_card.T)
+print(output_has_flow_card.shape)
+print(output_no_flow_card.T)
+print(output_no_flow_card.shape)
+
+# Save files
+output_has_flow_card.to_csv(main_file_path / 'output_files/P2024Week1_withFlowCard.csv', index=False)
+output_no_flow_card.to_csv(main_file_path / 'output_files/P2024Week1_withoutFlowCard.csv', index=False)
