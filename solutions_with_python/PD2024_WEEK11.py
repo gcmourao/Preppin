790939b4141691a80d78e016f6dda7c883ed67af
Gabriela Mourão <38528326+gcmourao@users.noreply.github.com>
2025-03-19 09:29:30 +0400

    Solution for week 11 challenge
    Challenge: create a new calendar

---
+++ solutions_with_python/PD2024_WEEK11.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+
+# Creating a list with all days from 2024
+days_2024 = pd.date_range(start="2024-01-01", end="2024-12-31", freq="D")
+days_list = days_2024.strftime("%Y-%m-%d").tolist()
+
+# Create new date:
+new_days_list = []
+month_calc = 0
+day_calc = 0
+for i in range(len(days_list)):
+    day_calc = (i+1) - 28*month_calc
+    new_days_list.append(f"2024-{month_calc+1}-{day_calc}")
+    if day_calc == 28:
+        month_calc += 1
+
+# Check answer:
+print(f"Number of months in the new calendar: {month_calc+1}")
+print(f"Last two days of the year in the new calendar: {new_days_list[-2:]}")
+
+
+
+
+
+#
